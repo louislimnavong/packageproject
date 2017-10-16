ae82df011ca9006bfe8459b50e36f5a4a64fbda6
Louis Limnavong <louislimnavong@MacBook-Pro-de-Louis.local>
2017-10-16 17:24:39 +0200

    Add assert that criteria to my_csv_reader.

--- R/my_csv_reader.R
+++ R/my_csv_reader.R
@@ -1,16 +1,21 @@
 #' import multiple csv files
 #'
 #' @param path path to the folder
+#' @import assertthat
 #'
 #' @return a list
 #' @export
 #' @importFrom utils read.csv2
 #'
 #' @examples
-#'
+#' \dontrun{
 #' my_csv_reader("../../path/to/folder")
+#' }
 
 my_csv_reader <- function(path){
+  assert_that(is.dir(dirname(path)))
+  assert_that(is.writeable(dirname(path)))
+  assert_that(is.readable(dirname(path)))
   files <- list.files(path, pattern = "*.csv$", full.names = TRUE )
   result <- lapply(files,read.csv)
   return(result)
