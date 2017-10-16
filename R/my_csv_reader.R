acd3e41ea6ad61bfe36dc9d5249b5ab8d73640b8
Louis Limnavong <louislimnavong@MacBook-Pro-de-Louis.local>
2017-10-16 15:04:07 +0200

    Created the script for the csv reader function; updated the Roxygen skeleton.

--- R/my_csv_reader.R
+++ R/my_csv_reader.R
@@ -1,7 +1,17 @@
-my_csv_reader <- function(path="/Users/louislimnavong/Desktop/MSc Data Science for Business/X/MAP536 - R/X_2017/Jour 1/data"){
+#' import multiple csv files
+#'
+#' @param path path to the folder
+#'
+#' @return a list
+#' @export
+#' @importFrom utils read.csv2
+#'
+#' @examples
+#'
+#' my_csv_reader("../../path/to/folder")
+
+my_csv_reader <- function(path){
   files <- list.files(path, pattern = "*.csv$", full.names = TRUE )
   result <- lapply(files,read.csv)
   return(result)
 }
-my_csv_reader()
-```
