a701139a75720d1762378477951c9451b0aa2af0
Louis Limnavong <louislimnavong@MacBook-Pro-de-Louis.local>
2017-10-16 16:42:07 +0200

    Updated the devtools_history in the data_raw file.

--- data-raw/devtools_history.R
+++ data-raw/devtools_history.R
@@ -1,1 +1,8 @@
 devtools::use_data_raw()
+devtools::use_package("dplyr")
+devtools::use_package("assertthat")
+devtools::use_package("forcats")
+devtools::use_package("ggplot2")
+devtools::use_package("dygraphs")
+devtools::use_package("tidyr")
+
