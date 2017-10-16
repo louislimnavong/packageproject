8e9338b738a669b895940b8f900e52898ee019e7
Louis Limnavong <louislimnavong@MacBook-Pro-de-Louis.local>
2017-10-16 15:39:00 +0200

    Created the script for the save as csv function. Also need to add dyplr to check is the function works.

---
+++ R/save_as_csv.R
@@ -0,0 +1,24 @@
+#' Save a data set as a csv file
+#'
+#' @param dataset dataset to save as csv
+#' @param filename filename to save under
+#' @param row.names
+#' @param ...
+#'
+#' @return csv file
+#' @export
+#'
+#' @examples
+
+save_as_csv <- function(dataset, filename, row.names = FALSE, ...) {
+  assert_that(has_extension(filename, "csv"))
+  assert_that(is.dir(dirname(filename)))
+  assert_that(is.writeable(dirname(filename)))
+  assert_that(not_empty(dataset))
+  assert_that(is.data.frame(dataset))
+
+  write.csv2(x = dataset, file = filename, row.names = row.names,...)
+
+  invisible(normalizePath(filename)) # instead of return
+
+}
