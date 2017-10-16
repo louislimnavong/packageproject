91bb7ff3cb653787548c8515bda357be9ca90778
Louis Limnavong <louislimnavong@MacBook-Pro-de-Louis.local>
2017-10-16 16:24:44 +0200

    Created the script for the draw_a_name function.

---
+++ R/draw_a_name.R
@@ -0,0 +1,25 @@
+#' Plot the evolution of the numbers of time per year a name was given since 1900
+#'
+#' @param the_name the name to plot
+#' @param the_sex the sex related to the name
+#'
+#' @import ggplot2
+#' @import dplyr
+#' @import dygraphs
+#' @import tidyr
+#'
+#' @return a plot
+#' @export
+#'
+#' @examples
+#' draw_a_name(the_name = "Vincent",the_sex = "M")
+
+draw_a_name <- function(the_name,the_sex){
+  prenoms %>%
+    filter(name == the_name,sex == the_sex) %>%
+    group_by(year) %>%
+    summarise(n=sum(n,na.rm=TRUE)) %>%
+    ggplot(aes(year,n)) + geom_line() +
+    ggtitle(paste(the_name)) + ggthemes::theme_gdocs()
+
+}
