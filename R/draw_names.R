059adaa64f62d5ea378a5d1cacce3102b807f8ac
Louis Limnavong <louislimnavong@MacBook-Pro-de-Louis.local>
2017-10-16 16:19:01 +0200

    Created the script for the function draw_names.

---
+++ R/draw_names.R
@@ -0,0 +1,27 @@
+#' Plot a graph comparing the evolutions of the frequency of two given names
+#'
+#' @param names the two names to compare
+#'
+#' @import ggplot2
+#' @import dplyr
+#' @import dygraphs
+#' @import tidyr
+#'
+#' @return a plot
+#' @export
+#'
+#' @examples
+#' draw_names(c("Diane","Vincent"))
+
+draw_names <- function(names){
+
+  prenoms::prenoms %>%
+    group_by(year,name) %>%
+    summarise(total = sum(n))    %>%
+
+    filter(name %in% names) %>%
+    ggplot(aes(year,total,color=name))+geom_line() +
+    # ggtitle(paste(names,collapse = ";"))+
+    ggthemes::theme_gdocs()
+
+}
