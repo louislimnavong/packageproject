2a96f6de7bdced2288ffb0300519386b6dfcd10c
Louis Limnavong <louislimnavong@MacBook-Pro-de-Louis.local>
2017-10-16 16:49:56 +0200

    Add assert that criteria for read_excel_multi

--- R/read_excel_multi .R
+++ R/read_excel_multi .R
@@ -2,6 +2,7 @@
 #'
 #' @param file path to xlsx file
 #' @import readxl
+#' @import assertthat
 #' @return a list
 #' @export
 #'
@@ -11,6 +12,11 @@
 #' }
 
 read_excel_multi <- function(file) {
+  assert_that(has_extension(file, "xlsx"))
+  assert_that(is.dir(dirname(file)))
+  assert_that(is.writeable(dirname(file)))
+
+
   all_sheets <- readxl::excel_sheets(file)
   result <- lapply(all_sheets, function(sheet) {
     readxl::read_excel(file, sheet = sheet)
